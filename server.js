89b80cf74b135134f88185ec84628bcf6a95c818
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-10-10 09:42:49 +0100

    update

--- server.js
+++ server.js
@@ -129,15 +129,14 @@ app.get('/big-devs(.html)?', [cohort1, cohort2, cohort3, cohort4])
 
 app.all('/*', (req, res) => {
     res.status(404)
-    if (req.accepts('html')){
+    if (req.accepts('html')) {
     res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if {req.accept('json') {
+    } else if (req.accept('json')) {
         res.json({error: '404 Not found' });
     }else {
         res.type('txt').send('404 Not found')
     }
 })
-
 
 
 app.use(errorHandler)
